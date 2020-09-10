0b6f2e90cbff9522aab366bc3ac4975ad10c0682
Krishna Vivek Kolasani <vivekkolasani45@gmail.com>
2020-09-10 12:18:31 +0530

    UI Stage 2 Complete

--- src/components/PassGen.jsx
+++ src/components/PassGen.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { motion } from "framer-motion";
 
 // Styles and Assets
@@ -6,6 +6,21 @@ import styles from "./PassGen.module.scss";
 import Logo from "../assets/icons/logo.png";
 
 const PassGen = () => {
+  const [settings, setSettings] = useState({
+    isVisible: false,
+    useNumbers: true,
+    useSymbols: true,
+    useUppercase: true,
+    useLowercase: true,
+    passwordLength: 14,
+  });
+
+  const toggleSettingsVisibility = () => {
+    let tempSettings = { ...settings };
+    tempSettings.isVisible = !tempSettings.isVisible;
+    setSettings(tempSettings);
+  };
+
   return (
     <div className={styles.PassGen}>
       <div className={styles.Container}>
@@ -17,10 +32,21 @@ const PassGen = () => {
             <p>2S7m@X4dpZ$7Czk8</p>
           </div>
         </div>
-        <motion.div className={styles.OptionsContainer}>
-          <motion.div className={styles.OptionsHeader}>
+        <motion.div className={styles.SettingsContainer}>
+          <motion.div
+            className={styles.SettingsHeader}
+            onClick={toggleSettingsVisibility}
+          >
             <p>settings</p>
-            <svg fill="none" viewBox="0 0 24 24">
+            <motion.svg
+              fill="none"
+              viewBox="0 0 24 24"
+              initial={{ rotate: 0, transformOrigin: "center" }}
+              animate={{
+                rotate: settings.isVisible ? 90 : 0,
+                transformOrigin: "center",
+              }}
+            >
               <path
                 fill="currentColor"
                 d="M11.0858 7.75739L15.3284 12L11.0858 16.2427L9.67157 14.8285L12.5 12L9.67157 9.1716L11.0858 7.75739Z"
@@ -31,8 +57,13 @@ const PassGen = () => {
                 fill="currentColor"
                 d="M12 1C5.92487 1 1 5.92487 1 12C1 18.0751 5.92487 23 12 23C18.0751 23 23 18.0751 23 12C23 5.92487 18.0751 1 12 1ZM21 12C21 7.02944 16.9706 3 12 3C7.02944 3 3 7.02944 3 12C3 16.9706 7.02944 21 12 21C16.9706 21 21 16.9706 21 12Z"
               />
-            </svg>
+            </motion.svg>
           </motion.div>
+          <motion.div
+            className={styles.Settings}
+            initial={{ height: "0vh" }}
+            animate={{ height: settings.isVisible ? "14vh" : "0vh" }}
+          ></motion.div>
         </motion.div>
         <div className={styles.Controllers}>
           <div className={styles.Copy}>
