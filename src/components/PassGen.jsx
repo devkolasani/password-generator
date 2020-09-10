a612ae898155e9cd420c1f16c050eae5202842f7
Krishna Vivek Kolasani <vivekkolasani45@gmail.com>
2020-09-10 15:55:10 +0530

    UI Stage 3 Complete

--- src/components/PassGen.jsx
+++ src/components/PassGen.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 
 // Styles and Assets
 import styles from "./PassGen.module.scss";
@@ -7,7 +7,7 @@ import Logo from "../assets/icons/logo.png";
 
 const PassGen = () => {
   const [settings, setSettings] = useState({
-    isVisible: false,
+    isVisible: true,
     useNumbers: true,
     useSymbols: true,
     useUppercase: true,
@@ -41,10 +41,11 @@ const PassGen = () => {
             <motion.svg
               fill="none"
               viewBox="0 0 24 24"
-              initial={{ rotate: 0, transformOrigin: "center" }}
+              initial={{ rotate: 0, originX: "50%", originY: "50%" }}
               animate={{
                 rotate: settings.isVisible ? 90 : 0,
-                transformOrigin: "center",
+                originX: "50%",
+                originY: "50%",
               }}
             >
               <path
@@ -52,8 +53,8 @@ const PassGen = () => {
                 d="M11.0858 7.75739L15.3284 12L11.0858 16.2427L9.67157 14.8285L12.5 12L9.67157 9.1716L11.0858 7.75739Z"
               />
               <path
-                fill-rule="evenodd"
-                clip-rule="evenodd"
+                fillRule="evenodd"
+                clipRule="evenodd"
                 fill="currentColor"
                 d="M12 1C5.92487 1 1 5.92487 1 12C1 18.0751 5.92487 23 12 23C18.0751 23 23 18.0751 23 12C23 5.92487 18.0751 1 12 1ZM21 12C21 7.02944 16.9706 3 12 3C7.02944 3 3 7.02944 3 12C3 16.9706 7.02944 21 12 21C16.9706 21 21 16.9706 21 12Z"
               />
@@ -61,9 +62,22 @@ const PassGen = () => {
           </motion.div>
           <motion.div
             className={styles.Settings}
-            initial={{ height: "0vh" }}
-            animate={{ height: settings.isVisible ? "14vh" : "0vh" }}
-          ></motion.div>
+            initial={{ height: "0vh", display: "none" }}
+            animate={{
+              height: settings.isVisible ? "14vh" : "0vh",
+              display: settings.isVisible ? "flex" : "none",
+            }}
+          >
+            <div className={styles.Setting}>
+              <label className={styles.CheckBox}>
+                <input type="checkbox" />
+                <svg viewBox="0 0 21 21">
+                  <polyline points="5 10.75 8.5 14.25 16 6"></polyline>
+                </svg>
+                <span>uppercase</span>
+              </label>
+            </div>
+          </motion.div>
         </motion.div>
         <div className={styles.Controllers}>
           <div className={styles.Copy}>
